04092a6fe7beb5a77c0e16c4c42734a90c31cf9a
Katerine-18 <ramirezkaterine250@gmail.com>
2025-08-05 20:50:11 -0600

    Update EjecutarOpcionRecepcionista.cs
    se actualizo codigo

--- EjecutarOpcionRecepcionista.cs
+++ EjecutarOpcionRecepcionista.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SistemaGestionHotel
+namespace SISTEMA_GESTION_DE_HOTEL
 {
     // Clase cliente
     public class Cliente
